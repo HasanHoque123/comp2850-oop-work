c0e4ce5c21d166efea932117559ca5cee6701c37
HasanHoque123 <xgfl5561@leeds.ac.uk>
2025-10-24 17:05:52 +0000

    first function complete

--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,1 +1,10 @@
 // Implement the six required functions here
+
+fun sValid(word: String): Boolean {
+    if (word.length == 5) {
+        return true
+    } else {
+        return false
+    }
+
+}
