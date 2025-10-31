a44abf0765b1f054ae845fd8bc304436aa689936
HasanHoque123 <xgfl5561@leeds.ac.uk>
2025-10-31 14:57:15 +0000

    try again innit

--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,10 +1,64 @@
 // Implement the six required functions here
+import java.io.File 
+import kotlin.random.Random 
 
-fun sValid(word: String): Boolean {
-    if (word.length == 5) {
+
+
+fun isValid(word: String): Boolean {
+    if(word.length == 5) {
         return true
     } else {
-        return false
+        return false 
+    }
+}
+
+fun readWordList(filename: String): MutableList<String> {
+    return File(filename).readLines().toMutableList()
+
+}
+
+
+fun pickRandomWord(words: MutableList<String>): String {
+    val randomWordIndex = Random.nextInt(words.size)
+    return words[randomWordIndex]
+
+}
+
+fun obtainGuess(attempt: Int): String {
+    while(true) {
+        println("attempt: $attempt")
+        val userInput = readln()
+        if (isValid(userInput)) {
+            return userInput
+        } else {
+            println("word is invalid try again")
+        }
+    } 
+}
+
+
+fun evaluateGuess(guess: String, target: String): List<Int> {
+    val comparisons = mutableListOf<Int>()
+    for(n in 0..4){
+        if (guess[n] == target[n]) {
+            comparisons[n] == 1
+        } else{
+            comparisons[n] == 0
+        }
     }
 
+    return comparisons
+
+
+}
+
+
+fun displayGuess(guess: String, matches: List<Int>) {
+    for (n in 0..4){
+        if (matches[n] == 1) {
+            print(guess[n])
+        } else{
+            println("?")
+        }
+    }
 }
--- portfolio/wordle/src/test/kotlin/WordleTest.kt
+++ portfolio/wordle/src/test/kotlin/WordleTest.kt
@@ -1,8 +1,60 @@
 import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import java.io.File
 
 @Suppress("unused")
 class WordleTest : StringSpec({
     // Write your tests here
+
+    "returns true if given word 5 letters" {
+        withClue("word = hasan") {isValid("hasan") shouldBe true}
+        withClue("word = neroo") {isValid("neroo") shouldBe true}
+        withClue("word = itcan") {isValid("itcan") shouldBe true}
+    }
+
+
+    "returns false if word is shorter than 5 letters" {
+        withClue("word = cat") {isValid("cat") shouldBe false}
+        withClue("word = hi") {isValid("hi") shouldBe false}
+        withClue("word = n") {isValid("n") shouldBe false}
+    }
+
+    "returns false if word is longer than 5 letters" {
+        withClue("word = edward") {isValid("edward") shouldBe false}
+        withClue("word = eduardo") {isValid("eduardo") shouldBe false}
+        withClue("word = superman") {isValid("superman") shouldBe false}
+    }
+
+
+    "returns correct list when valid word list file is given" {
+        
+        val tempFile = File.createTempFile("wordlist", ".txt")
+        tempFile.writeText(
+            """
+            apple
+            brain
+            chair
+            doubt
+            eager
+            """.trimIndent()
+        )
+
+        val result = readWordList(tempFile.absolutePath)
+
+        withClue("The function should return all lines as a mutable list") {
+            result shouldBe mutableListOf("apple", "brain", "chair", "doubt", "eager")
+        }
+
+        // Cleanup
+        tempFile.deleteOnExit()
+    }
+
+    "error when file does not exist" {
+        val invalidPath = "error_file.txt"
+        val exception = kotlin.runCatching {
+
+        }
+    }
+
 })
