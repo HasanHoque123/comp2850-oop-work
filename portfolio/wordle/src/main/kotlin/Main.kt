6cbe3990ffcfc359483107073abcdb59d28db31e
HasanHoque123 <xgfl5561@leeds.ac.uk>
2025-11-03 14:07:57 +0000

    fixed final if statement

--- portfolio/wordle/src/main/kotlin/Main.kt
+++ portfolio/wordle/src/main/kotlin/Main.kt
@@ -1,13 +1,12 @@
 fun main() {
 
-    println("Starting program...")
 
     //read all the words from file 
     val words = readWordList("/workspaces/comp2850-oop-work/portfolio/wordle/data/words.txt")
 
     //pick a random word that will be guessed
     val chosenWord = pickRandomWord(words)
-     println("Chosen word: $chosenWord")
+    println(chosenWord)
 
     println("guess a 5 letter word. You have 10 attempts")
 
@@ -19,7 +18,7 @@ fun main() {
         displayGuess(guess, matches)
 
         //check if all the characters match the chosen word 
-        if (matches.all  {it ==1}) {
+        if (0 !in matches){
             println("you guessed in $attempts goes")
             return
         }
